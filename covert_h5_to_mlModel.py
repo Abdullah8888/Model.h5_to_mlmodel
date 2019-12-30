1c55deabc8e93f9becd3352e3bfe2107d1747424
Jimoh Babatunde <jimoh.abdullahi@konga.com>
2019-12-30 17:28:27 +0100

    Updated

--- covert_h5_to_mlModel.py
+++ covert_h5_to_mlModel.py
@@ -1,13 +1,6 @@
 
 import coremltools
 
-# output_labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-# your_model = coremltools.converters.keras.convert('final_model.h5', input_names=['image'], output_names=['output'], class_labels=output_labels, image_input_names='image')
-# coreml_model = coremltools.converters.keras.convert('final_model.h5', input_names = 'image', image_input_names = 'image', class_labels = ['cat', 'dog'], output_names = 'output')
-# coreml_model.save('MyModel.mlmodel')
-# print("welcome")
-
-
 coreml_model = coremltools.converters.keras.convert('model.h5', input_names=['image'], output_names=['output'],image_input_names='image')
 
 coreml_model.author = 'Jimoh Babatunde'
